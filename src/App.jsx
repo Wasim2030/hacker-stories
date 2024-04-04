cffb52663f732fc018442817eaaf1886c729070a
wasim <wasimakhter2030@gmail.com>
2024-04-04 16:23:08 +0530

    add chapter: react add

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const App = () => {
   const stories = [
     {
@@ -17,28 +19,35 @@ const App = () => {
       objectID: 1,
     },
   ];
-
+  console.log("🚀 ~ App ~ App:");
   return (
     <div>
       <h1>Hello World</h1>
       <SearchComponent />
       <hr />
       <List list={stories} />
+      <hr />
+      <Search />
     </div>
   );
 };
 
 export default App;
 
-const List = (props) => (
-  <ul>
-    {props.list.map((item) => (
-      <Item key={item.objectID} item={item} />
-    ))}
-  </ul>
-);
+const List = (props) => {
+  console.log("🚀 ~ list ~ list:");
+  return (
+    <ul>
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+};
 
 const Item = (props) => {
+  console.log("🚀 ~ Item ~ Item:");
+
   return (
     <li>
       <span>
@@ -51,18 +60,67 @@ const Item = (props) => {
   );
 };
 
+// const SearchComponent = () => {
+//   const [a, seta] = useState("");
+//   const handleChange = (event) => {
+//     // synthetic event
+//     // console.log(event);
+//     // value of target (here: input html element)
+//     // console.log(event.target.value);
+//     let val = event.target.value;
+//     // console.log(val);
+//     // return val;
+//     seta(val);
+//   };
+
+//   return (
+//     <>
+//       <label htmlFor="search">Search: </label>
+//       <input type="text" id="search" onChange={handleChange} />
+//       <h1>{a}</h1>
+//     </>
+//   );
+// };
+
 const SearchComponent = () => {
   const handleChange = (event) => {
-    // synthetic event
-    console.log(event);
-    // value of target (here: input html element)
     console.log(event.target.value);
   };
+  console.log("🚀 ~ SearchComponent ~ SearchComponent:");
 
   return (
     <>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" onChange={handleChange} />
     </>
+  );
+};
+
+const Search = () => {
+  /*
+  // Doesnot word this way
+
+  let searchTerm = "";
+  const handleChange = (event) => {
+    searchTerm = event.target.value;
+    console.log(searchTerm);
+  };
+  */
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  console.log("🚀 ~ Search ~ Search:");
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" onChange={handleChange} />
+
+      <p>
+        Searching for: <strong>{searchTerm}</strong>
+      </p>
+    </div>
   );
 };
