e038bd174df1f3ea976c8e0f0d29f1584f5d6cba
wasim <wasimakhter2030@gmail.com>
2024-04-05 09:16:53 +0530

    chapter: call back handler in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,99 +19,30 @@ const App = () => {
       objectID: 1,
     },
   ];
-  console.log("🚀 ~ App ~ App:");
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>Hello World</h1>
-      <SearchComponent />
+      <Search onSearch={handleSearch} />
       <hr />
       <List list={stories} />
       <hr />
-      <Search />
     </div>
   );
 };
 
 export default App;
 
-const List = (props) => {
-  console.log("🚀 ~ list ~ list:");
-  return (
-    <ul>
-      {props.list.map((item) => (
-        <Item key={item.objectID} item={item} />
-      ))}
-    </ul>
-  );
-};
-
-const Item = (props) => {
-  console.log("🚀 ~ Item ~ Item:");
-
-  return (
-    <li>
-      <span>
-        <a href={props.item.url}>{props.item.title}</a>
-        <p>{props.item.author}</p>
-        <p>{props.item.num_comments}</p>
-        <p>{props.item.points}</p>
-      </span>
-    </li>
-  );
-};
-
-// const SearchComponent = () => {
-//   const [a, seta] = useState("");
-//   const handleChange = (event) => {
-//     // synthetic event
-//     // console.log(event);
-//     // value of target (here: input html element)
-//     // console.log(event.target.value);
-//     let val = event.target.value;
-//     // console.log(val);
-//     // return val;
-//     seta(val);
-//   };
-
-//   return (
-//     <>
-//       <label htmlFor="search">Search: </label>
-//       <input type="text" id="search" onChange={handleChange} />
-//       <h1>{a}</h1>
-//     </>
-//   );
-// };
-
-const SearchComponent = () => {
-  const handleChange = (event) => {
-    console.log(event.target.value);
-  };
-  console.log("🚀 ~ SearchComponent ~ SearchComponent:");
-
-  return (
-    <>
-      <label htmlFor="search">Search: </label>
-      <input type="text" id="search" onChange={handleChange} />
-    </>
-  );
-};
-
-const Search = () => {
-  /*
-  // Doesnot word this way
-
-  let searchTerm = "";
-  const handleChange = (event) => {
-    searchTerm = event.target.value;
-    console.log(searchTerm);
-  };
-  */
-
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+    props.onSearch(event);
   };
-  console.log("🚀 ~ Search ~ Search:");
 
   return (
     <div>
@@ -122,5 +53,28 @@ const Search = () => {
         Searching for: <strong>{searchTerm}</strong>
       </p>
     </div>
+  );
+};
+
+const List = (props) => {
+  return (
+    <ul>
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+const Item = (props) => {
+  return (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+        <p>{props.item.author}</p>
+        <p>{props.item.num_comments}</p>
+        <p>{props.item.points}</p>
+      </span>
+    </li>
   );
 };
